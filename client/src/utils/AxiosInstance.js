27fd5e530b5848fae737183b83bc720815d79b8f
shamroz73 <shamrozwarraich@gmail.com>
2022-04-05 23:51:43 +0200

    client url update

--- client/src/utils/AxiosInstance.js
+++ client/src/utils/AxiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axioscall = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://notify-me-mern-app-backend.vercel.app/api/",
 });
